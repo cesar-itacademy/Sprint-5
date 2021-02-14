7edc6a31ef0a9446e51da695cbca0af1bc9355b4
cesar-itacademy <cesar-itacademy@users.noreply.github.com>
2021-02-14 22:07:37 +0100

    Level2  Afegida API info metereológica

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,34 +1,49 @@
 // WEB ACUDITS
-// Nivell 1
+// Nivell 2
 
-/* Exercici 1
-Crear la web d'acudits, el funcionament dels quals és:
+/* Exercici 3
+Ben fet! Ja tens una web d'acudits operativa. Ja que està web està pensada per a mostrar acudits als usuaris a primera hora del matí perquè comencin bé el dia, afegirem informació meteorològica ja que els pot ser d'utilitat. 
 
-- En entrar no mostrarà cap acudit. Apareixerà el títol i el botó de següent acudit“
+Consumir una API d'informació meteorològica i mostrar-ho en la web. Aquesta API ha de dir-se en l'obertura, no mitjançant un botó.
 
-- En prémer el botó de “Següent acudit” es farà fetch a la API d'acudits i es mostrarà per consola l'acudit en qüestió.
-
-Nota: En aquest exercici no és necessari maquetar la web, primer farem que funcioni per a passar a aplicar-li els estils.
-
-Tip 1: usar promises o async/await per a esperar la resposta de la API
-
-Tip 2: abans d'usar una API en el codi, és recomanable usar Postman o eines online per a provar la API, per exemple https://apitester.com/. A més de garantir que funciona, veuràs l'objecte que retorna, per a saber utilitzar-lo.
-EXERCICI 1 I 2 */
+Nota: Encara no és necessari maquetar la web, amb mostrar una paraula que indiqui el temps és suficient. */
 
 const button = document.getElementById("button");
 
-button.addEventListener("click", () => {
-    fetch("https://icanhazdadjoke.com", {
-      method: "GET",
-      headers: {
-        Accept: "application/json"
-      }
-    })
-    .then((response) => response.ok ? Promise.resolve(response) : Promise.reject(response))
-    .then((response) => response.json())
-    .then((dataJoke) => {
-      console.log(dataJoke.joke); // Exercici 1
-      const paragraph = document.getElementById("joke");
-      paragraph.textContent = dataJoke.joke; // Exercici 2
-    })
-});
+// Funció que mostra el temps carrega desde l'inici 
+ window.onload = function temps() {
+   // API KEY no inclosa per no publicarla al Github
+   const url = "https://api.openweathermap.org/data/2.5/weather?lang=ca&id=3128760&appid={API}";
+   fetch(url)
+     .then((response) => response.ok ? Promise.resolve(response) : Promise.reject(response)) // Comprovam si la resposta ha estat OK
+     .then((response) => response.json()) // Treiem les dades del promise com JSON
+     .then((dataWeather) => {
+       const weatherParagraph = document.getElementById("temps");
+       // Mostram el temps que fa a la ciutat de Barcelona 
+       weatherParagraph.textContent = `${(dataWeather.weather[0].description).toUpperCase()} A ${(dataWeather.name).toUpperCase()}`;
+     })
+     // Alerta que indica que la resposta del servidor API no ha estat satisfactoria
+     .catch(response => alert(`API Key no inclosa per securetat`))
+     // .catch(response => alert(response)) 
+ };
+
+
+// Funció que carrega un nou acudit al fer click al botó
+ button.addEventListener("click", () => {
+   const url = "https://icanhazdadjoke.co";
+    fetch(url, {
+       method: "GET",
+       headers: {
+         Accept: "application/json"
+       }
+     })
+      .then((response) => response.ok ? Promise.resolve(response) : Promise.reject(response))// Comprovam si la resposta ha estat OK
+      .then((response) => response.json())// Treiem les dades del promise com JSON
+      .then((dataJoke) => {
+        // Mostram un nou acudit 
+        const paragraph = document.getElementById("joke");
+        paragraph.textContent = dataJoke.joke;
+      })
+      // Alerta que indica que la resposta del servidor API no ha estat satisfactoria
+      .catch(response => alert(response)) 
+ });
